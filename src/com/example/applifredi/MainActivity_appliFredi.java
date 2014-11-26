dbed264da486be0c1b82772bd28949d8b6ba1ca2
Mamas62 <malik.lela.62@gmail.com>
2014-11-26 11:32:44 +0100

    essai de creer un passage d'une vue a d'une autre mais reste erreurs

--- src/com/example/applifredi/MainActivity_appliFredi.java
+++ src/com/example/applifredi/MainActivity_appliFredi.java
@@ -1,9 +1,12 @@
 package com.example.applifredi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_appliFredi extends Activity {
 
@@ -11,5 +14,23 @@ public class MainActivity_appliFredi extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_appli_fredi);
+        
+        final Button btnValider = (Button) findViewById(R.id.btnValider);
+        btnValider.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+          Intent intent = new Intent(MainActivity_appliFredi.this, passageVue.class);
+          startActivity(intent);
+          }
+        });
+        }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
+ 
 }
