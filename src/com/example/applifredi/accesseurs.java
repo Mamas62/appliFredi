9ad6cc3017fd6a8aa92e022d92ef0557c2560b86
Mamas62 <malik.lela.62@gmail.com>
2014-12-01 09:31:01 +0100

    suppression puis creation accesseurs

---
+++ src/com/example/applifredi/accesseurs.java
@@ -0,0 +1,48 @@
+package com.example.applifredi;
+
+public class accesseurs {
+
+	private double id;
+	private String nom, prenom, adresse;
+	
+	public accesseurs(double id,String nom,String prenom,String adresse){
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+	}
+	
+	// ACCESEURS
+	
+	public double getId(){	
+		return this.id;	
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public String getPrenom(){
+		return this.prenom;
+	}
+
+	public String getAdresse(){
+		return this.adresse;
+	}
+	
+	// MODIFICATEURS
+	
+	public void setNom(String nom){
+		this.nom = nom;
+	}
+	
+	public void setPrenom(String prenom){
+		this.prenom = prenom;		
+	}
+	
+	public void setAdresse(String adresse){
+		this.adresse = adresse;
+	}
+	
+
+}
