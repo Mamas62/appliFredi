a3ed27fea897bc63dc0560bcea0a6ae0ca8889d7
Mamas62 <malik.lela.62@gmail.com>
2014-12-01 16:00:59 +0100

    supression pagecreationfraisdeplacement pour creer activity android et donc modification dans passageVue du layout

---
+++ src/com/example/applifredi/MainActivity_pagecreationfraisdeplacement.java
@@ -0,0 +1,15 @@
+package com.example.applifredi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity_pagecreationfraisdeplacement extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_activity_pagecreationfraisdeplacement);
+	}
+}
--- src/com/example/applifredi/passageVue.java
+++ src/com/example/applifredi/passageVue.java
@@ -7,7 +7,7 @@ public class passageVue extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pagecreationfraisdeplacement);
+        setContentView(R.layout.activity_main_activity_pagecreationfraisdeplacement);
     }
  
     /*@Override
