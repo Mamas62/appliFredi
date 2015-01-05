a041502078de91b188cdffcc3cc83ac046ec4623
Mamas62 <malik.lela.62@gmail.com>
2015-01-05 14:08:14 +0100

    index passage vue vers historique

---
+++ src/com/example/applifredi/passageVueVersHistorique.java
@@ -0,0 +1,16 @@
+package com.example.applifredi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class passageVueVersHistorique extends Activity {
+	 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_historique);
+        
+        
+    }
+  
+}
